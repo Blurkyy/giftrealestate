f99d3bc33b8b536d8308a6f90672eac9c169fdff
Blurkyy <michaelmesfin202@gmail.com>
2026-06-17 22:43:10 +0300

    telegram user change

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -121,7 +121,7 @@ export default function Footer() {
               <Instagram className="w-5 h-5" />
             </a>
             <a
-              href="https://t.me/liegehomes"
+              href="@AdsisHomesFinder"
               className="w-10 h-10 rounded-full bg-gray-800 hover:bg-primary-600 flex items-center justify-center transition"
               aria-label="Telegram"
               target="_blank"
